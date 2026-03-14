9303c1c6286e5ad115113d185753590bca944df4
dangit <dangit27092005@gmail.com>
2026-03-14 14:42:13 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -122,3 +122,31 @@ Route::get('/admin/report/dashboard', function () {
 Route::get('/admin/report/create', function () {
     return view('Admin.thongke.create');
 });
+
+// #QUANLYDANHMUC
+// Route::get('/admin/danhmuc', function () {
+//     return view('Admin.quanlydanhmuc.index');
+// });
+
+// Route::get('/admin/danhmuc/create', function () {
+//     return view('Admin.quanlydanhmuc.create');
+// });
+
+// Route::get('/admin/danhmuc/edit', function () {
+//     return view('Admin.quanlydanhmuc.edit');
+// });
+
+// ///CHỈNH SỬA HỒ SƠ CÁ NHÂN
+// Route::get('/hoso/chinhsua', function () {
+//     return view('Giangvien.hosocanhan.edit');
+// });
+
+// ///PHÊ DUYỆT ĐỀ XUẤT
+// Route::get('/pheduyetdexuat', function () {
+//     return view('Admin.pheduyetdexuat.index');
+// });
+
+// //THEO DÕI TIẾN ĐỒ ĐỀ TÀI
+// Route::get('/theodoitiendo', function () {
+//     return view('Admin.theodoitiendo.index');
+// });
