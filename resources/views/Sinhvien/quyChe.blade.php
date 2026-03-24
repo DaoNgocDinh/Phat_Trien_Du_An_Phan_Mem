6b6f5b698413b545f8eca9f505a61a0b2f7bcc98
Tên của bạn <dinhdaongoc18@gmail.com>
2026-03-24 20:35:11 +0700

    'SuaLoiVat2'

--- resources/views/Sinhvien/quyChe.blade.php
+++ resources/views/Sinhvien/quyChe.blade.php
@@ -66,14 +66,6 @@
                                             clip-rule="evenodd" />
                                     </svg>
                                 </a>
-                                <a href="{{ route('download.pdf', $quyche->FilePDF) }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor"
-                                        class="size-8 p-1 rounded-full hover:bg-gray-200 text-green-500 cursor-pointer">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                                    </svg>
-                                </a>
                             </div>
                         </td>
                     </tr>
