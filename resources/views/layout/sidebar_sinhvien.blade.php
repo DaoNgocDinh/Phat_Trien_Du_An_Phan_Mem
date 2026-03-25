3eb17b07338bea66a5c5ca87a3c0bb4fbae7de35
Vũ Đăng <101622939+vuhaidangz@users.noreply.github.com>
2026-03-25 15:17:28 +0700

    a

--- resources/views/layout/sidebar_sinhvien.blade.php
+++ resources/views/layout/sidebar_sinhvien.blade.php
@@ -53,7 +53,7 @@
                 <li>
                     <a href="{{ route('sinhvien.suKien') }}"
                         class="flex items-center px-3 py-2.5 text-white rounded-md hover:bg-[#2c5d6e] hover:shadow-md transition-all duration-200 
-                       {{ request()->routeIs('sinhvien.suKien.*') ? 'bg-[#2c5d6e] shadow-md border-l-4 border-white text-white font-semibold' : '' }}">
+                       {{ request()->routeIs('sinhvien.suKien') ? 'bg-[#2c5d6e] shadow-md border-l-4 border-white text-white font-semibold' : '' }}">
                         <i class="fas fa-calendar-check w-6 text-lg"></i>
                         <span class="ml-3">Sự kiện</span>
                     </a>
